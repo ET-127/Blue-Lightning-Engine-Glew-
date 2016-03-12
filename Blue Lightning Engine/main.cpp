6a8ef78ca7dc412410ad76d7050a4c7c1b5cb4e4
ET127 <Eyitayo@Eyitayos-MacBook-Pro.local>
2016-03-12 19:35:53 +0000

    Initial Commit

---
+++ Blue Lightning Engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Blue Lightning Engine
+//
+//  Created by Eyitayo Adedeji on 12/03/2016.
+//  Copyright (c) 2016 Eyitayo Adedeji. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
