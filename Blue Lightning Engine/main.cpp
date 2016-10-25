c8ba16a7bb3ebf0ef80a216c3c40e885c58c4688
ET-127 <eyitt1@gmail.com>
2016-10-25 01:33:02 +0100

    Stuff
    More stuff

--- Blue Lightning Engine/main.cpp
+++ Blue Lightning Engine/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <vector>
 #include <gl/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -14,6 +15,21 @@ using namespace std;
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+
+const float halfScreenWidth = SCREEN_WIDTH / 2.0f;
+const float halfScreenHeight = SCREEN_HEIGHT / 2.0f;
+
+void keyCallback(GLFWwindow *window,int key,int scancode,int action,int mods);
+void characterCallback(GLFWwindow *window,unsigned int keyCode);
+void characterModCallback(GLFWwindow *window,unsigned int keyCode,int modifierKey);
+static void cursorPosCallBack(GLFWwindow *window,double xPos,double yPos);
+void cursorEnterCallBack(GLFWwindow *window,int entered);
+void mouseButtonCallback(GLFWwindow *window,int button,int action,int mods);
+void DrawCube(GLfloat centerPosX,GLfloat centerPosY,GLfloat centerPosZ,GLfloat edgeLength); 
+
+double timeSinceStart;
+float rotationX;
+float rotationY;
 
 void DrawRect(float sizeX,float sizeY){
     
@@ -29,7 +45,30 @@ void DrawRect(float sizeX,float sizeY){
     
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(3, GL_FLOAT, 0, vertices);
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
     glDrawArrays(GL_QUADS, 0, 4);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    
+}
+
+void DrawPoly(int sides){
+    
+    //vector<GLfloat> vertices;
+    GLfloat vertices[] = {
+        
+        0,0,0,
+        0,150,0,
+        20,100,0,
+        40,150,0,
+        60,0,0
+        
+    };
+    
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+    glDrawArrays(GL_POLYGON, 0, 5);
     glDisableClientState(GL_VERTEX_ARRAY);
     
 }
@@ -52,7 +91,7 @@ void DrawTriangle(float size){
     
 }
 
-void DrawPoint(float x,float y,float size){
+void DrawPoint(float x,float y,float size,bool isCircular){
     
     GLfloat point[] = {
         
@@ -60,10 +99,131 @@ void DrawPoint(float x,float y,float size){
         
     };
     
+    if(isCircular) {
+        
+        glEnable(GL_POINT_SMOOTH);
+        
+    }
+    
     glEnableClientState(GL_VERTEX_ARRAY);
-    glPointSize(50);
+    glPointSize(size);
     glVertexPointer(2, GL_FLOAT, 0, point);
-    glDrawArrays(GL_POINT, 0, 1);
+    glDrawArrays(GL_POINTS, 0, 1);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    
+    if(isCircular) {
+        
+        glDisable(GL_POINT_SMOOTH);
+        
+    }
+    
+}
+
+void DrawCube(GLfloat centerPosX,GLfloat centerPosY,GLfloat centerPosZ,GLfloat edgeLength){
+    
+    GLfloat halfSideLength = edgeLength * 0.5f;
+    
+    GLfloat vertices[] = {
+        
+        //Front z is always +
+        centerPosX - halfSideLength, centerPosY + halfSideLength,centerPosZ + halfSideLength, //Top Left
+        centerPosX + halfSideLength, centerPosY + halfSideLength,centerPosZ + halfSideLength, //Top Right
+        centerPosX + halfSideLength, centerPosY - halfSideLength,centerPosZ + halfSideLength, //Bottom Right
+        centerPosX - halfSideLength, centerPosY - halfSideLength,centerPosZ + halfSideLength, //Bottom Left
+        
+        //Back z is always -
+        centerPosX - halfSideLength, centerPosY + halfSideLength,centerPosZ - halfSideLength, //Top Left
+        centerPosX + halfSideLength, centerPosY + halfSideLength,centerPosZ - halfSideLength, //Top Right
+        centerPosX + halfSideLength, centerPosY - halfSideLength,centerPosZ - halfSideLength, //Bottom Right
+        centerPosX - halfSideLength, centerPosY - halfSideLength,centerPosZ - halfSideLength, //Bottom Left
+        
+        ///////
+        
+        //Left x is always -
+        centerPosX - halfSideLength, centerPosY + halfSideLength,centerPosZ + halfSideLength, //Top Left
+        centerPosX - halfSideLength, centerPosY + halfSideLength,centerPosZ - halfSideLength, //Top Right
+        centerPosX - halfSideLength, centerPosY - halfSideLength,centerPosZ - halfSideLength, //Bottom Right
+        centerPosX - halfSideLength, centerPosY - halfSideLength,centerPosZ + halfSideLength, //Bottom Left
+        
+        //Right x is always +
+        centerPosX + halfSideLength, centerPosY + halfSideLength,centerPosZ + halfSideLength, //Top Left
+        centerPosX + halfSideLength, centerPosY + halfSideLength,centerPosZ - halfSideLength, //Top Right
+        centerPosX + halfSideLength, centerPosY - halfSideLength,centerPosZ - halfSideLength, //Bottom Right
+        centerPosX + halfSideLength, centerPosY - halfSideLength,centerPosZ + halfSideLength, //Bottom Left
+        
+        ///////
+        
+        //Top y is always +
+        centerPosX - halfSideLength, centerPosY + halfSideLength,centerPosZ + halfSideLength, //Top Left
+        centerPosX - halfSideLength, centerPosY + halfSideLength,centerPosZ - halfSideLength, //Top Right
+        centerPosX + halfSideLength, centerPosY + halfSideLength,centerPosZ - halfSideLength, //Bottom Right
+        centerPosX + halfSideLength, centerPosY + halfSideLength,centerPosZ + halfSideLength, //Bottom Left
+        
+        //Bottom y is always -
+        centerPosX - halfSideLength, centerPosY - halfSideLength,centerPosZ + halfSideLength, //Top Left
+        centerPosX - halfSideLength, centerPosY - halfSideLength,centerPosZ - halfSideLength, //Top Right
+        centerPosX + halfSideLength, centerPosY - halfSideLength,centerPosZ - halfSideLength, //Bottom Right
+        centerPosX + halfSideLength, centerPosY - halfSideLength,centerPosZ + halfSideLength //Bottom Left
+        
+        
+    };
+    
+    GLfloat colours[] = {
+        
+        255,0,0,
+        255,0,0,
+        255,0,0,
+        255,0,0,
+        
+        0,255,0,
+        0,255,0,
+        0,255,0,
+        0,255,0,
+        
+        0,0,255,
+        0,0,255,
+        0,0,255,
+        0,0,255,
+        
+        255,255,0,
+        255,255,0,
+        255,255,0,
+        255,255,0,
+        
+        255,0,255,
+        255,0,255,
+        255,0,255,
+        255,0,255,
+        
+        0,255,255,
+        0,255,255,
+        0,255,255,
+        0,255,255
+        
+    };
+    
+  
+    //Draw a coloured cube
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+    glColorPointer(3, GL_FLOAT, 0, colours);
+    
+    glDrawArrays(GL_QUADS, 0, 24);
+    
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    
+    //Draw a wireframe cube ontop of it
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+    
+    glDrawArrays(GL_QUADS, 0, 24);
+    
     glDisableClientState(GL_VERTEX_ARRAY);
     
 }
@@ -91,6 +251,20 @@ int main( void ) {
     //Create a window + OpenGL Context
     window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Blue Lightning Engine", NULL, NULL);
     
+    //Set Keyboard Mode
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
+    
+    //Set Cursor Mode
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    
+    //Set all the input callbacks
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetCharCallback(window, characterCallback);
+    glfwSetCharModsCallback(window, characterModCallback);
+    glfwSetCursorPosCallback(window, cursorPosCallBack);
+    glfwSetCursorEnterCallback(window, cursorEnterCallBack);
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    
     if(!window){
         
         cout << "Window Initialisation Error!" << endl;
@@ -109,14 +283,21 @@ int main( void ) {
     glLoadIdentity(); // Replaces current matrix with identity matrix
     
     //Left;Right;Bottom;Top;Near;Far
-    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 2000);
+
     //Defines how objects are transformed(e.g translated and rotated)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     
+    cout << "Initilises time = " << glfwGetTime() << endl;
+    
     cout << "Starting game loop..." << endl;
+    glfwSetTime(0);
     //Game Loop
     while (!glfwWindowShouldClose(window)) {
+        
+        timeSinceStart = glfwGetTime();
+        //cout << timeSinceStart << endl;
         
         glClear(GL_COLOR_BUFFER_BIT);
         
@@ -124,21 +305,27 @@ int main( void ) {
         
         /*
         
-        1.Tell OpenGl we want to draw a vertex Array
+        1.Tell OpenGl we want to draw something e.g a Vertex Array
          
         2.Size of Each Vertex(3D = 3,2D = 2,1D = 1) ;Value type; Vertex Array
          
         3.DrawType ;Where to Start; Number of Vertices
          
-        4.Tell OpenGl we are done drawing the vertex array
+        4.Tell OpenGl we are done drawing the thing e.g a Vertex Array
      
         */
-        
-        DrawPoint(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 50);
 
-        //DrawRect(50, 150);
+        glPushMatrix();
+        glTranslatef(halfScreenWidth, halfScreenHeight, -500);
+        glRotatef(rotationX, 1, 0, 0);
+        glRotatef(rotationY, 0, 1, 0);
+        glTranslatef(-halfScreenWidth, -halfScreenHeight, 500);
         
-        glDisableClientState(GL_VERTEX_ARRAY);
+        DrawCube(halfScreenWidth, halfScreenHeight, -500, 200);
+        
+        DrawCube(halfScreenWidth, halfScreenHeight, -500, 200);
+        
+        glPopMatrix();
         
         //Swap front and back buffers
         glfwSwapBuffers(window);
@@ -154,3 +341,107 @@ int main( void ) {
     return 0;
     
 }
+
+
+
+
+//Cursor Position
+static void cursorPosCallBack(GLFWwindow *window,double xPos,double yPos){
+    
+    //cout << "X: " << xPos << " Y: " << yPos << endl;
+    
+}
+
+//Whether mouse has entered or exited the window
+void cursorEnterCallBack(GLFWwindow *window,int entered){
+    
+    if(entered){
+        
+        //cout << "Mouse has entered window" << endl;
+        
+    } else {
+        
+        //cout << "Mouse has left window" << endl;
+        
+    }
+    
+}
+
+void mouseButtonCallback(GLFWwindow *window,int button,int action,int mods){
+    
+    //cout << button << endl;
+    
+}
+
+
+//Character entered
+void characterCallback(GLFWwindow *window,unsigned int keyCode){
+   
+    //cout << (char)keyCode << endl;
+    
+}
+
+//Character + Special entered
+void characterModCallback(GLFWwindow *window,unsigned int keyCode,int modifierKey){
+    
+    cout << (char)keyCode << " : " << modifierKey << endl;
+    
+}
+
+//All Key Presses ,Held and Released
+void keyCallback(GLFWwindow *window,int key,int scancode,int action,int mods){
+    
+    const GLfloat rotationSpeed = 10;
+    
+    switch (key) {
+            
+        case GLFW_KEY_UP:
+            
+            rotationX -= rotationSpeed;
+            break;
+            
+        case GLFW_KEY_DOWN:
+            
+            rotationX += rotationSpeed;
+            break;
+            
+        case GLFW_KEY_LEFT:
+            
+            rotationY += rotationSpeed;
+            break;
+            
+        case GLFW_KEY_RIGHT:
+            
+            rotationY -= rotationSpeed;
+            break;
+            
+        default:
+            
+            break;
+            
+    }
+    
+    /*switch (action) {
+            
+        case GLFW_PRESS:
+            
+            cout << (char)key << " was pressed!" << endl;
+        
+            break;
+            
+        case GLFW_REPEAT:
+            
+            cout << (char)key << " is being held!" << endl;
+            break;
+            
+        case GLFW_RELEASE :
+            
+            cout << (char)key << " was released!" << endl;
+            break;
+            
+        default:
+            break;
+    }*/
+    
+}
+
